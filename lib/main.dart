78b29c1594018d2bfd93f2941a7becd139417bb2
GeniuStone <dugustlr@naver.com>
2023-02-07 16:18:46 +0900

    BottomAppBar 수정

--- lib/main.dart
+++ lib/main.dart
@@ -50,27 +50,18 @@ class MyApp extends StatelessWidget {
             ],
           ),
         ),
-      bottomNavigationBar: BottomAppBar(child: MainBottom()),
+      bottomNavigationBar: BottomNavigationBar(
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home_outlined), label: '홈'),
+          BottomNavigationBarItem(icon: Icon(Icons.shopping_bag_outlined), label: '샵'),
+        ],
+      ),
       );
   }
 }
 
-class MainBottom extends StatelessWidget {
-  MainBottom({Key? key}) : super(key: key);
 
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.fromLTRB(0, 5, 0, 5),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          IconButton(onPressed: (){}, icon: Icon(Icons.home_outlined)),
-          IconButton(onPressed: (){}, icon: Icon(Icons.shopping_bag_outlined)),
-        ],
-      ),
-    );
-  }
-}
 
 
