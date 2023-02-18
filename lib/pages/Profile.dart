cec672ee9d9e5c96f2808be6f5f67fbb7f3a224f
GeniuStone <dugustlr@naver.com>
2023-02-18 15:51:14 +0900

    프로필 페이지 뒤로가기 버튼 추가

--- lib/pages/Profile.dart
+++ lib/pages/Profile.dart
@@ -9,6 +9,16 @@ class Profile extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        leading: IconButton(
+          onPressed: (){
+            Navigator.pop(context);
+          },
+          icon: Icon(
+            Icons.arrow_back,
+            color: Colors.black,
+            size: 30,
+          ),
+        ),
         title: Text(context.watch<Store1>().name),
         centerTitle: true,
       ),
