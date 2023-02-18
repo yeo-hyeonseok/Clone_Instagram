51d6fa90733301feacf5c7a5fda8acd535f2c41f
GeniuStone <dugustlr@naver.com>
2023-02-18 15:35:36 +0900

    프로필 페이지 상단 레이아웃 생성

--- lib/pages/Profile.dart
+++ lib/pages/Profile.dart
@@ -10,13 +10,27 @@ class Profile extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text(context.watch<Store1>().name),
+        centerTitle: true,
       ),
-      body: Column(
-        children: [
-          ElevatedButton(onPressed: (){
-            context.read<Store1>().setName('차차무식');
-          }, child: Text("버튼")),
-        ],
+      body: Container(
+        padding: EdgeInsets.fromLTRB(20, 20, 20, 0),
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(30),
+                  child: Image.asset('assets/images/profile.jpg', width: 60, height: 60, fit: BoxFit.cover,),
+                ),
+                Text('팔로워 0명', style: TextStyle(
+                  fontSize: 16
+                ),),
+                ElevatedButton(onPressed: (){}, child: Text('팔로우'))
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
